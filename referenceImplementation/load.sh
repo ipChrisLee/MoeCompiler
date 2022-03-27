e5ea062f5be683b654cfd33af28e0a8ae287015c
19211332-ChrisLee <19211332@bjtu.edu.cn>
2022-03-27 18:19:08 +0800

    Changed load.sh

--- referenceImplementation/load.sh
+++ referenceImplementation/load.sh
@@ -1,2 +1,11 @@
-echo SpinCat by BJTU in CSC2020
-git clone https://gitlab.eduxiji.net/CSC2020-BJTU-SpinCat/compiler.git ./SpinCat-BJTU-CSC2020
+#!/bin/bash
+__CYAN='\033[0;36m'
+__GREEN='\033[0;32m'
+__NOC='\033[0m'
+
+projDirSpinCat=./SpinCat-BJTU-CSC2020
+if ! [ -d $projDirSpinCat ];then
+    echo -e "$__CYAN"Loading \"SpinCat\" by BJTU in CSC2020"$__NOC"
+    git clone https://gitlab.eduxiji.net/CSC2020-BJTU-SpinCat/compiler.git $projDirSpinCat
+fi
+echo -e "$__GREEN"Finished."$__NOC"
