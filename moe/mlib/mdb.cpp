22b9eaeb62688ba3e80e9ff6f11628cc90b9f215
19211332-ChrisLee <19211332@bjtu.edu.cn>
2022-04-22 16:55:55 +0800

    Delete third_party/SysYTestCase/

--- moe/mlib/mdb.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory> 
-
-#include "mdb.hpp"
-#include "cprt.hpp"
-
-
-bool com::mdb::sysEnable=false;
-com::mdb com::ccdbg(std::make_unique<com::cprt>(std::cout,com::cprt::blue),false,"[ccdbg] ");
-
--- moe/pyScript/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-conda env update --file MoePyEnvConfig.yml
-
