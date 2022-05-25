12f3d3bf50c259739095fcdf95d00b03bf01e79c
19211332-ChrisLee <19211332@bjtu.edu.cn>
2022-05-25 18:04:15 +0800

    mtype changed

--- moe/src/mlib/mtype.cpp
+++ moe/src/mlib/mtype.cpp
@@ -2,7 +2,7 @@
 #include "submain.hpp"
 #include "cprt.hpp"
 
-namespace mtype{
+namespace mtype {
 quat_t qFalse(quat_t::QValue::False);
 quat_t qTrue(quat_t::QValue::True);
 quat_t qUndefined(quat_t::QValue::Undefined);
@@ -11,14 +11,14 @@ quat_t qChaos(quat_t::QValue::Chaos);
 
 
 AddSubMain(testquat_t,
-    [](const std::vector<std::string>&)->int{
-        using namespace mtype;
-        quat_t qT=qTrue;
-        quat_t qF=qFalse;
-        quat_t qU=qUndefined;
-        quat_t qC=qChaos;
-        com::ccout.cprintLn(std::tuple(qF," ",qT," ",qU," ",qC));
-        com::ccout.cprintLn(std::tuple(qF&&qT," ",qF&&qF," ",qT&&qT," ",qU&&qT," ",qC&&qT));
-        return 0;
-    }
+           [](const std::vector<std::string> &) -> int {
+	           using namespace mtype;
+	           quat_t qT = qTrue;
+	           quat_t qF = qFalse;
+	           quat_t qU = qUndefined;
+	           quat_t qC = qChaos;
+	           com::ccout.cprintLn(std::tuple(qF, " ", qT, " ", qU, " ", qC));
+	           com::ccout.cprintLn(std::tuple(qF && qT, " ", qF && qF, " ", qT && qT, " ", qU && qT, " ", qC && qT));
+	           return 0;
+           }
 );
--- moe/src/mlib/mtype.hpp
+++ moe/src/mlib/mtype.hpp
@@ -6,47 +6,68 @@
 
 #include "moeconcept.hpp"
 
-namespace mtype{
+namespace mtype {
 
 class quat_t : public moeconcept::Stringable {
   public:
-    enum class QValue{ False,True,Undefined,Chaos }qvalue;
-    quat_t (QValue qv):qvalue(qv){}
-
-    quat_t():qvalue(QValue::Undefined){}
-    quat_t(bool b){
-        if(b) qvalue=QValue::True;
-        else qvalue=QValue::False;
-    }
-    quat_t(const quat_t &) = default;
-    quat_t & operator = (const quat_t &) = default;
-    bool operator == (const quat_t & q){ return qvalue==q.qvalue; }
-    quat_t operator && (const quat_t & q) { 
-        if(isChaos() || q.isChaos()) return QValue::Chaos;
-        if(isUndefined() || q.isUndefined()) return QValue::Undefined;
-        if(isTrue() && q.isTrue()) return QValue::True;
-        return QValue::False;
-    }
-    quat_t operator ! () {
-        if(isTrue()) return QValue::False;
-        if(isFalse()) return QValue::True;
-        return *this;
-    }
-    bool isUndefined() const { return qvalue==QValue::Undefined; }
-    bool isTrue() const { return qvalue==QValue::True; }
-    bool isFalse() const { return qvalue==QValue::False; }
-    bool isChaos() const { return qvalue==QValue::Chaos; }
-    virtual std::string toString() const override {
-        switch (qvalue) {
-            case QValue::False      : return "qFalse";
-            case QValue::True       : return "qTrue";
-            case QValue::Undefined  : return "qUndefined";
-            case QValue::Chaos      : return "qChaos";
-        }
-    }
-    friend std::ostream & operator << (std::ostream & os,const quat_t & q){
-        os<<q.toString();return os;
-    }
+	
+	enum class QValue {
+		False, True, Undefined, Chaos
+	} qvalue;
+	
+	quat_t(QValue qv) : qvalue(qv) { } // NOLINT
+	
+	quat_t() : qvalue(QValue::Undefined) { }
+	
+	explicit quat_t(bool b) {
+		if (b) qvalue = QValue::True;
+		else qvalue = QValue::False;
+	}
+	
+	quat_t(const quat_t &) = default;
+	
+	quat_t & operator =(const quat_t &) = default;
+	
+	bool operator ==(const quat_t & q) const { return qvalue == q.qvalue; }
+	
+	quat_t operator &&(const quat_t & q) const {
+		if (isChaos() || q.isChaos()) return QValue::Chaos;
+		if (isUndefined() || q.isUndefined()) return QValue::Undefined;
+		if (isTrue() && q.isTrue()) return QValue::True;
+		return QValue::False;
+	}
+	
+	quat_t operator !() {
+		if (isTrue()) return QValue::False;
+		if (isFalse()) return QValue::True;
+		return *this;
+	}
+	
+	[[nodiscard]] bool isUndefined() const { return qvalue == QValue::Undefined; }
+	
+	[[nodiscard]] bool isTrue() const { return qvalue == QValue::True; }
+	
+	[[nodiscard]] bool isFalse() const { return qvalue == QValue::False; }
+	
+	[[nodiscard]] bool isChaos() const { return qvalue == QValue::Chaos; }
+	
+	[[nodiscard]] std::string toString() const override {
+		switch (qvalue) {
+			case QValue::False      :
+				return "qFalse";
+			case QValue::True       :
+				return "qTrue";
+			case QValue::Undefined  :
+				return "qUndefined";
+			case QValue::Chaos      :
+				return "qChaos";
+		}
+	}
+	
+	friend std::ostream & operator <<(std::ostream & os, const quat_t & q) {
+		os << q.toString();
+		return os;
+	}
 };
 
 extern quat_t qFalse;
