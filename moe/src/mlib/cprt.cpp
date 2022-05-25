172b13baacfde9941cc5b8dc98e7ec11402297ab
19211332-ChrisLee <19211332@bjtu.edu.cn>
2022-05-25 17:30:59 +0800

    `cprt` changed

--- moe/src/mlib/cprt.cpp
+++ moe/src/mlib/cprt.cpp
@@ -1,24 +1,26 @@
 #include "cprt.hpp"
 
-com::cprt::TOS com::cprt::tos=cprt::TOS();
+com::cprt::TOS com::cprt::tos = cprt::TOS();
 
-namespace com{
-    const std::string cprt::black=   "\033[0;30m";
-    const std::string cprt::red=     "\033[0;31m";
-    const std::string cprt::err=     "\033[0;31m";
-    const std::string cprt::green=   "\033[0;32m";
-    const std::string cprt::fin=     "\033[0;32m";
-    const std::string cprt::yellow=  "\033[0;33m";
-    const std::string cprt::iterct=  "\033[0;33m";
-    const std::string cprt::blue=    "\033[0;34m";
-    const std::string cprt::purple=  "\033[0;35m";
-    const std::string cprt::cyan=    "\033[0;36m";
-    const std::string cprt::working= "\033[0;36m";
-    const std::string cprt::bpurple= "\033[1;35m";
-    const std::string cprt::warning= "\033[1;35m";
-    const std::string cprt::noc=     "\033[0m";
-    cprt ccout(std::cout,cprt::iterct);
-    cprt ccerr(std::cerr,cprt::err);
-    const std::string nullFilePath("/nul/dev");
-    cprt cnull(nullFilePath);
+namespace com {
+namespace color {
+const std::string black = "\033[0;30m";
+const std::string red = "\033[0;31m";
+const std::string err = "\033[0;31m";
+const std::string green = "\033[0;32m";
+const std::string fin = "\033[0;32m";
+const std::string yellow = "\033[0;33m";
+const std::string iterct = "\033[0;33m";
+const std::string blue = "\033[0;34m";
+const std::string purple = "\033[0;35m";
+const std::string cyan = "\033[0;36m";
+const std::string working = "\033[0;36m";
+const std::string bpurple = "\033[1;35m";
+const std::string warning = "\033[1;35m";
+const std::string noc = "\033[0m";
+}
+cprt ccout(std::cout, color::iterct);
+cprt ccerr(std::cerr, color::err);
+const std::string nullFilePath("/nul/dev");
+cprt cnull(nullFilePath);
 }
