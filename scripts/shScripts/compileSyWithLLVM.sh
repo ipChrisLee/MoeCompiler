cb7848df4a25f6174a262185318330a215e88e3c
19211332-ChrisLee <19211332@bjtu.edu.cn>
2022-07-18 20:42:37 +0800

    Before adding passes

--- scripts/shScripts/compileSyWithLLVM.sh
+++ scripts/shScripts/compileSyWithLLVM.sh
@@ -1,16 +1,15 @@
+#!/bin/bash
 syBaseFilePath="testDir/testSyFile/test"
 
+set -x
 clang -Xclang -disable-O0-optnone -O0 -S -x c -emit-llvm \
   -target armv7m-unknown-unknown-unknown \
   -fno-discard-value-names \
-  $syBaseFilePath".sy" -o $syBaseFilePath".ll"
+  $syBaseFilePath".sy" -o $syBaseFilePath".ll" || touch $syBaseFilePath".ll"
 
-retval=$?
-if [ $retval -ne 0 ]; then
-  touch $syBaseFilePath".ll"
-  exit 0
-fi
+opt -S $syBaseFilePath".ll" -o $syBaseFilePath".m2r.ll" -debug-pass=Executions \
+  -reg2mem -mem2reg
 
-opt -S -mem2reg $syBaseFilePath".ll" -o $syBaseFilePath".m2r.ll"
+#opt -S $syBaseFilePath".ll" -o $syBaseFilePath".O2.ll" -debug-pass=Arguments -O2
 
 llc $syBaseFilePath".ll" -o $syBaseFilePath".s"
